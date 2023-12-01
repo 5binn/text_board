a3ef741f37365c3125ce507989aa893cfa38f29d
SBS <dhxnejqls13@naver.com>
2023-12-01 18:07:01 +0900

    수정2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.print("Hello and welcome!!!!");
+        System.out.print("Hello and welcome!!");
 
 
     }
